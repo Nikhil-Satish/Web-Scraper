fc5838f52e97d52d5f45e976483faff0bee4c01e
Nikhil-Satish <77330133+Nikhil-Satish@users.noreply.github.com>
2021-11-28 11:52:17 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -1,119 +1,105 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
 const express = require('express')
+const cors = require('cors')
 const app = express()
-const url = 'https://indianexpress.com/'
-axios(url)
-  .then(response =>{
-    const html = response.data
-    const $ = cheerio.load(html)
-    
-    const home = []
-    const title = $('.ie-logos',html).find('a').attr('title')
-    home.push(title)
-    console.log(title);
 
-    const date = $('.datefollow .datebox',html).text()
-    home.push(date)
-    console.log(date);
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+var url = ''
+app.use(cors())
+var jsonData = {}
 
-    var contactUrl = 'https://indianexpress.com'+$('#menu-item-6454144').find('a').attr('href')
-    home.push(contactUrl)
-    console.log("Contact details");
-    console.log(contactUrl);
+app.post('/',async function (req, res){
+  url = req.body.text
+  console.log('Got the url: '+url);
+  // await putData(url)
+  console.log('Before putData');
+  await putData(url)
+  console.log('After put data');
+  console.log(jsonData);
+  // res.json('Hi')
+    // console.log(jsonData.title);
+    res.send(jsonData)
+    // res.send(jsonData)
+})
 
-    var subscribe = $('#menu-item-6454146').find('a').attr('href')
-    home.push(subscribe)
-    console.log("Subscription link");
-    console.log(subscribe);
+async function putData(url){
+  await axios(url)
+    .then(response =>{
+      const html = response.data
+      const $ = cheerio.load(html)
 
-    const socialMediaLinks = []
-    $('.g-follow-icon.g-follow li').each(function(){
-      const link = $(this).find('a').attr('href')
-      socialMediaLinks.push(link)
-    })
-    home.push(socialMediaLinks)
-    console.log("Social Media Links");
-    console.log(socialMediaLinks);
+      const home = []
+      const title = $('.ie-logos',html).find('a').attr('title')
 
-    app.get('/', function (req, res) {
-      res.json(home)
-    })
+      const date = $('.datefollow .datebox',html).text()
 
-    const topNews = []
+      var contactUrl = 'https://indianexpress.com'+$('#menu-item-6454144').find('a').attr('href')
 
-    const newsGenre = []
-    $('h2').each(function(){
-      const head = $(this).text()
-      newsGenre.push(head)
-    })
-    console.log("Topics");
-    console.log(newsGenre);
+      var subscribe = $('#menu-item-6454146').find('a').attr('href')
 
-    app.get('/categories', function (req, res) {
-        res.json(newsGenre)
-    })
-
-    $('.top-news h3 ').each(function(){
-      const news = $(this).text()
-      const link = $(this).find('a').attr('href')
-      topNews.push({
-        news,
-        link
+      const socialMediaLinks = []
+      $('.g-follow-icon.g-follow li').each(function(){
+        const link = $(this).find('a').attr('href')
+        socialMediaLinks.push(link)
       })
-    })
-    console.log("Top News");
-    console.log(topNews);
-    app.get('/topnews', function (req, res) {
-        res.json(news)
-    })
 
-    const entertainment = []
-    $('#HP_ENTERTAINMENT h4').each(function(){
-      const title = $(this).text()
-      const url = $(this).find('a').attr('href')
-      entertainment.push({
-        title,
-        url
+      const newsGenre = []
+      $('h2').each(function(){
+        const head = $(this).text()
+        newsGenre.push(head)
       })
-    })
-    app.get('/entertainment', function (req, res) {
-        res.json(entertainment)
-    })
-    console.log("Entertainment news");
-    console.log(entertainment);
 
-    const sports = []
-    $('#HP_IMAGE_ALREADY_ADDED h4').each(function(){
-      const title = $(this).text()
-      const url = $(this).find('a').attr('href')
-      sports.push({
-        title,
-        url
+      // const topNews = []
+      // const genreClass = ['.top-news h3 ','#HP_ENTERTAINMENT h4']
+      const notUsed = ['#HP_LATEST_VIDEO h4','#HP_VISUAL_STORIES h4','#HP_LATEST_PHOTOS h4','#HP_AUDIO h4',]
+
+      const array = ['.ie-first-story.m-premium','.top-news h3 ','#HP_LATEST_NEWS .top-news h3','#Bangalore .news h4',
+      '#HP_ONLY_IN_THE_EXPRESS h4', '#HP_ENTERTAINMENT h4',
+      '#HP_LATEST_VIDEO h4','#HP_VISUAL_STORIES h4','#HP_LATEST_PHOTOS h4','#HP_AUDIO h4',
+       '#HP_IMAGE_ALREADY_ADDED h4','#HP_TECHNOLOGY h4','#HP_TRENDING h4','#HP_EDUCATION h4',
+      '#HP_OPINION h3']
+
+      const newsCollection = []
+      array.forEach(element =>{
+        const newsTitle = []
+        $(element).each(function(){
+          const news = $(this).text()
+          const link = $(this).find('a').attr('href')
+          newsTitle.push({
+            news,
+            link,
+          })
+        })
+        newsCollection.push(newsTitle)
       })
-    })
-    app.get('/sports', function (req, res) {
-        res.json(sports)
-    })
-    console.log("Sports news");
-    console.log(sports);
 
-    const tech = []
-    $('#HP_TECHNOLOGY h4').each(function(){
-      const title = $(this).text()
-      const url = $(this).find('a').attr('href')
-      tech.push({
-        title,
-        url
-      })
-    })
-    app.get('/tech', function (req, res) {
-        res.json(tech)
-    })
-    console.log("Technology");
-    console.log(tech);
+      const spare = {
+        title : title,
+        date : date,
+        contactUrl : contactUrl,
+        subscribe : subscribe,
+        socialMediaLinks : socialMediaLinks,
+        newsGenre : newsGenre,
+        newsCollection : newsCollection
+      }
 
-  }).catch(err=>console.log(err))
+      jsonData = spare
+
+    }).catch(err=>console.log(err))
+}
+
+
+
 app.listen(3000,function(){
   console.log("Server started on port 3000");
 });
+
+// app.get('/', function (req, res) {
+//   // res.json(home)
+//   // console.log(req);
+//   console.log('Data came to get');
+//   res.json(spare)
+// })
